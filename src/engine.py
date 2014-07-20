35a64650473d17d97a49a0f23422f902eabb7baa
FlyGeneticist <flygeneticist@gmail.com>
2014-07-20 09:41:52 -0400

    refactored game engine. save/load using picked engine, not raw game files.

--- src/engine.py
+++ src/engine.py
@@ -6,72 +6,12 @@ from src.item import Item
 class Engine(object):
     def __init__(self):
         self.world_map = None
-        self.game_intro()
-        self.pick_loader()
-
-    def game_intro(self):
-        print("\n")
-        print("     _______ _________ _______  _______    _______  _______      \n")
-        print("    (  ____ )\__   __/(  ____ \(  ____ \  (  ___  )(  ____ \     \n")
-        print("    | (    )|   ) (   | (    \/| (    \/  | (   ) || (    \/     \n")
-        print("    | (____)|   | |   | (_____ | (__      | |   | || (__         \n")
-        print("    |     __)   | |   (_____  )|  __)     | |   | ||  __)        \n")
-        print("    | (\ (      | |         ) || (        | |   | || (           \n")
-        print("    | ) \ \_____) (___/\____) || (____/\  | (___) || )           \n")
-        print("    |/   \__/\_______/\_______)(_______/  (_______)|/            \n")
-        print("\n")
-        print("\n")
-        print("_________          _______    ______   _______  _______  ______  \n")
-        print("\__   __/|\     /|(  ____ \  (  __  \ (  ____ \(  ___  )(  __  \ \n")
-        print("   ) (   | )   ( || (    \/  | (  \  )| (    \/| (   ) || (  \  )\n")
-        print("   | |   | (___) || (__      | |   ) || (__    | (___) || |   ) |\n")
-        print("   | |   |  ___  ||  __)     | |   | ||  __)   |  ___  || |   | |\n")
-        print("   | |   | (   ) || (        | |   ) || (      | (   ) || |   ) |\n")
-        print("   | |   | )   ( || (____/\  | (__/  )| (____/\| )   ( || (__/  )\n")
-        print("   )_(   |/     \|(_______/  (______/ (_______/|/     \|(______/ \n")
-        print("\n")
-        print("\n")
-        print("~"*100)
-        print("This game will challenge you to escape from a\nhospital and resuce your family! Good luck!!")
-        print("~"*100 + '\n')
-        
-
-    def pick_loader(self):    
-        # get player input and load the correct game start
-        print("Please from choose from one of the following options:\n1) Start a new game\n2) Load a saved game")
-        pick = int(input("Enter your choice: ").strip())
-        if pick == 1:
-            self.start_new_game()
-        elif pick == 2:
-            name = input("Enter your character's name: ").strip()
-            self.load_saved_game(name)
-            print("Sorry! Loading / saving games are not available now!")
-            exit()
-        else:
-            print("That is not a valid choise. Please try again.\n")
-            self.pick_loader()
-
-
-    def load_saved_game(self, name): 
-        '''Copies saved game data to the TEMP folder based on the character name used at it's creation 
-        from a save folder of the same name'''
-        pass
-
+        self.start_new_game()
 
     def start_new_game(self):
         self.player = Player() # create the main player object
-        self.load_area('./worlds/world000.py', 's0') # first world map
-        
-
-    def end_game(self):
-        print("You have died. How unfortunate. Better luck next time!")
-        exit()
-
-
-    def win_game(self):
-        print("You made it out alive!\nYou win!!")
-        exit()
-
+        self.load_area('./src/worlds/world000.py', 's0') # first world map
+    
 
     def load_area(self, area_layout, start_scene):
         del self.world_map # free up memory by releasing old map/scene data
@@ -81,14 +21,16 @@ class Engine(object):
 
     def serve(self, scene): 
         if scene == "death":
+            print("You have died. How unfortunate. Better luck next time!")
             self.end_game()
         elif scene == "win":
-            self.win_game()
+            print("You made it out alive!\nYou win!!")
+            self.end_game()
         elif scene == None:
             return print("You cannot move there.")
         elif scene.split('_')[0] == "map":
             data = scene.split('_')
-            self.load_area('worlds/world' + data[1] + '.py', data[2])
+            self.load_area('./src/worlds/world' + data[1] + '.py', data[2])
         else:
             self.current_scene = self.world_map.next_scene(scene)
             return print(self.current_scene.description)
@@ -153,27 +95,3 @@ class Engine(object):
     def examine(self, command):
         return self.current_scene.examine_item(command)
 
-
-    def save_game(self):
-        '''Copies all game data files in TEMP folder into a saved game folder, based on player\'s name'''
-        save = input("Are you sure you want to overwrite the previously saved game? y/n: ").strip().lower()
-        if save == 'y':
-            # do some file shuffling here.....
-            print("Your game has been saved!")
-        elif save == 'n':
-            pass
-        else:
-            print("Not a valid option. Please try again.")
-            self.save_game()
-
-
-    def exit_game(self):
-        quit = input("Are you sure you want to exit the game? y/n: ").strip().lower()
-        if quit == 'y':
-            print("Thanks for playing!")
-            exit()
-        elif quit == 'n':
-            pass
-        else:
-            print("Not a valid option. Please try again.")
-            self.exit_game()
--- zombie-game.py
+++ zombie-game.py
@@ -1,12 +1,103 @@
 from src.engine import Engine
+import pickle
 
-# create the game engine to start the game
-e = Engine()
+
+def game_intro():
+    print("\n")
+    print("     _______ _________ _______  _______    _______  _______      \n")
+    print("    (  ____ )\__   __/(  ____ \(  ____ \  (  ___  )(  ____ \     \n")
+    print("    | (    )|   ) (   | (    \/| (    \/  | (   ) || (    \/     \n")
+    print("    | (____)|   | |   | (_____ | (__      | |   | || (__         \n")
+    print("    |     __)   | |   (_____  )|  __)     | |   | ||  __)        \n")
+    print("    | (\ (      | |         ) || (        | |   | || (           \n")
+    print("    | ) \ \_____) (___/\____) || (____/\  | (___) || )           \n")
+    print("    |/   \__/\_______/\_______)(_______/  (_______)|/            \n")
+    print("\n")
+    print("\n")
+    print("_________          _______    ______   _______  _______  ______  \n")
+    print("\__   __/|\     /|(  ____ \  (  __  \ (  ____ \(  ___  )(  __  \ \n")
+    print("   ) (   | )   ( || (    \/  | (  \  )| (    \/| (   ) || (  \  )\n")
+    print("   | |   | (___) || (__      | |   ) || (__    | (___) || |   ) |\n")
+    print("   | |   |  ___  ||  __)     | |   | ||  __)   |  ___  || |   | |\n")
+    print("   | |   | (   ) || (        | |   ) || (      | (   ) || |   ) |\n")
+    print("   | |   | )   ( || (____/\  | (__/  )| (____/\| )   ( || (__/  )\n")
+    print("   )_(   |/     \|(_______/  (______/ (_______/|/     \|(______/ \n")
+    print("\n")
+    print("\n")
+    print("~"*100)
+    print("This game will challenge you to escape from a\nhospital and resuce your family! Good luck!!")
+    print("~"*100 + '\n')
+    
+
+def pick_loader():    
+    # get player input and load the correct game start
+    print("Please from choose from one of the following options:\n1) Start a new game\n2) Load a saved game")
+    try:
+        pick = int(input("Enter your choice: ").strip())
+    except:
+        print("That is not a valid choice. Please try again.\n")
+        pick_loader()
+    else:
+        if pick == 1:
+            return start_new_game()
+        elif pick == 2:
+            name = input("Enter your character's name: ").strip()
+            return load_saved_game(name)
+        else:
+            print("That is not a valid choice. Please try again.\n")
+            pick_loader()
+
+
+def start_new_game():
+    return Engine()
+
+
+def load_saved_game(name): 
+    '''Loads pickled game engine from the saves folder. Load the last map and scene.'''
+    with open(r"./saves/"+name+".pickle", "rb") as input_file:
+        return pickle.load(input_file)
+
+
+def save_game():
+    '''Copies all game data files in TEMP folder into a saved game folder, based on player\'s name'''
+    save = input("Are you sure you want to proceed?\nThis will overwrite any previously saved game data? y/n: ").strip().lower()
+    if save == 'y':
+        with open(r"./saves/" + e.player.name + ".pickle", "wb") as output_file:
+            pickle.dump(e, output_file)
+        print("Your game has been saved!")
+    elif save == 'n':
+        pass
+    else:
+        print("Not a valid option. Please try again.")
+        save_game()
+
+
+def exit_game():
+    quit = input("Are you sure you want to exit the game? y/n: ").strip().lower()
+    if quit == 'y':
+        print("Thanks for playing!")
+        end_game()
+    elif quit == 'n':
+        pass
+    else:
+        print("Not a valid option. Please try again.")
+        exit_game()
+
+
+def end_game():
+    exit()
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Run-time commands to handle passing player input back into the game engine.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+game_intro()
+e = pick_loader()
 
 # quick lookup dict for all valid player input actions linked back to the engine function
 valid_actions = {   "look":e.look,"move":e.move,"take":e.take,"drop":e.drop, 
                     "find":e.find, "check":e.check_inv, "examine":e.examine,
-                    "save":e.save_game, "exit":e.exit_game
+                    "save":save_game, "exit":exit_game
                 }
 
 while (True):
@@ -16,6 +107,9 @@ while (True):
     # check that the action has two words (verb and object)
     if (action[0] in valid_actions) and (len(action) == 2):
         valid_actions[action[0]](action[1])
+    # chekc if action is save or exit commands in don't use an arg
+    elif (action[0] == 'save') or (action[0] == 'exit'):
+        valid_actions[action[0]]()
     elif (action[0] == 'help'):
         print('The following are valid commands:\n',
                 'look <north, south, east, west> => Examines a given direction.\n',
